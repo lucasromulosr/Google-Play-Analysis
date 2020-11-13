b64e043615ecc10fb230797d79d7079a22314bef
srlucasromulo <lucas.romulo.sr@gmail.com>
2020-11-13 12:26:01 -0300

    definindo processo dos dados

--- program.py
+++ program.py
@@ -2,3 +2,36 @@
 # antes de fazer tudo na aplicação
 # vai ser deletado dps
 
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import sys
+
+def main():
+
+    # argumento é o id do aplicativo
+    # exmplo: com.github.android
+    arg = None
+    try:
+        arg = sys.argv[1]
+    except IndexError:
+        print('----------------\n'
+              'faltou argumento\n'
+              '----------------')
+        sys.exit()
+
+    ### pipeline
+    # passar id do aplicativo pro crawler
+    # resultado <- yield
+
+    # passar 'resultado' pra analysis
+    # resultado <- yield
+    ### fim pipeline
+
+    # banco de dados
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
